cf7d26bba866ec599ae14abe48f9afcf5769106f
edgardfn <edgardfinotti@yahoo.com.br>
2021-02-23 14:26:45 -0300

    Criado useEffect para salvar dados da Api no estado ao montar aplicação e importado constantes do axios no componente App para ser usado na requisição.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState} from "react";
+import api from './services/api'
 
 import {
   SafeAreaView,
@@ -11,7 +12,14 @@ import {
 } from "react-native";
 
 export default function App() {
-  const [projects, setProjects] = useState([])
+  const [repositories, setRepositories] = useState([])
+
+  useEffect(() => {
+    api.get('/repositories').then(response => {
+      console.log(response)
+      setRepositories(response.data)
+    })
+  }, [])
 
   async function handleLikeRepository(id) {
     // Implement "Like Repository" functionality
